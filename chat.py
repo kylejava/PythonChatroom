502ea55604c6861deb8f4305cbda75d9e0f500b5
Kyle <kyleedwardgjava@gmail.com>
2021-04-24 20:24:31 -0700

    Changed name of server

--- chat.py
+++ chat.py
@@ -6,5 +6,5 @@ server = socket(AF_INET, SOCK_DGRAM)
 server.bind(('' , serverPort))
 
 while True:
-    message, clientAddress = user2.recvfrom(2048)
+    message, clientAddress = server.recvfrom(2048)
     print(message.decode())
